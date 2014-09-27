9c6dd0bc7147373543c66d1e029d4f0b9aae99ee
GollyTicker <golly.ticker@gmail.com>
2014-09-27 14:12:38 +0200

    succ/1 not defined. using succ/2. add.pl not working

--- a1/a2_prolog_functioning.pl
+++ a1/a2_prolog_functioning.pl
@@ -1,15 +1,23 @@
 
 
 % successor.pl
-zahl(0).
-zahl(succ(X)):- zahl(X).
+%
+% zahl(0).
+% zahl(succ(X)):- zahl(X).
 
+% Überarbeitete Version mit succ/2
+zahl(0).
+zahl(Y) :- succ(X,Y), zahl(X).
 
 % addition_succ.pl
-add(0,Y,Y).
-add(succ(X),Y,succ(Z)) :-
-  add(X,Y,Z).
-
+% add(0,Y,Y).
+% add(succ(X),Y,succ(Z)) :-
+%  add(X,Y,Z).
+add(0, X, X).
+add(X, Y, Z) :-
+	succ(N, X),
+	succ(M, Z),
+	add(N, Y, M).
 
 % hanoi.pl
 transportiere(1, Von, _, Nach) :-
